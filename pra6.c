8c2955d8c68be1aadb2e935bc052eb5eb81d3e8b
yashviee <yashvigujarati10@gmail.com>
2025-09-13 16:42:06 +0530

    Add files via upload

---
+++ pra6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+   float  c,fe;
+   printf("enter the temperature value in c:\n") ;
+   scanf("%f",&c);
+   fe=(c*9/5)+32;
+   printf(" the ferenhit value: %f\n",fe) ;
+   return 0;
+
+}
