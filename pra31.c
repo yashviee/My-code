d8bc18f9303c49f0125fbbb83375e7d15b0d72bb
yashviee <yashvigujarati10@gmail.com>
2025-11-20 22:11:48 +0530

    Add files via upload

---
+++ pra31.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Aayesha Shaikh-25CE113\n");
+    char *summary;
+    int initialSize, newSize;
+
+    printf("Enter initial memory size for summary (in bytes): ");
+    scanf("%d", &initialSize);
+    getchar();
+
+    summary = (char *)calloc(initialSize, sizeof(char));
+
+    if (summary == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    printf("Enter initial article summary: ");
+    fgets(summary, initialSize, stdin);
+    printf("\nInitial Summary: %s\n", summary);
+    printf("Enter new memory size to expand summary (in bytes): ");
+    scanf("%d", &newSize);
+    getchar();
+    summary = (char *)realloc(summary, newSize);
+
+    if (summary == NULL) {
+        printf("Memory reallocation failed!\n");
+        return 1;
+    }
+
+    printf("Enter updated (longer) summary: ");
+    fgets(summary, newSize, stdin);
+
+    printf("\nUpdated Summary: %s\n", summary);
+
+    free(summary);
+
+    return 0;
+}
